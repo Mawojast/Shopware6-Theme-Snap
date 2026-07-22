74333f229d0f4523bd1ce57189c3ab83dfc17450
mawojast <inet.beck@gmail.com>
2026-07-22 12:22:26 +0200

    Initial commit

---
+++ src/Snap.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Snap;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Snap extends Plugin implements ThemeInterface
+{
+}
